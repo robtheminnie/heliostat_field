2a3fc218f6bda30617b00918ec0fa86a600e4bb4
robtheminnie <47690074+robtheminnie@users.noreply.github.com>
2022-02-15 16:21:18 +0000

    Update uart.py

--- heliostat/software/pi_pico/micropython/uart.py
+++ heliostat/software/pi_pico/micropython/uart.py
@@ -22,6 +22,7 @@ class uart_interface:
         self.tx_data_to_master = bytearray()
         self.tx_data_to_slaves = bytearray()
         self.led = machine.Pin(25, machine.Pin.OUT)
+    # end def
         
 
     def check_for_data(self):
@@ -30,6 +31,7 @@ class uart_interface:
             temp_buffer = bytearray()
             while self.uart_from_master.any() > 0:
                 temp_buffer += self.uart_from_master.read(1)
+            # end while
             
             if len(temp_buffer) == 5:
 
@@ -49,23 +51,27 @@ class uart_interface:
                         # it is, increment destination by 1, this is my ID, then forward to next slave
                         self.rx_data_from_master[1] += 1
                         self.myID = self.rx_data_from_master[1]
+                    #end if
                         
                     # this is not for me, pass it on
                     print("forward from master")
                     self.tx_data_to_slaves = self.rx_data_from_master
                     self.uart_to_slaves.write(self.tx_data_to_slaves)
+                #end if
             
     
         if self.uart_to_slaves.any() > 0:
             temp_buffer = bytearray()
             while self.uart_to_slaves.any() > 0:
                 temp_buffer += self.uart_to_slaves.read(1)
+            # end while
             
             if len(temp_buffer) == 5:
                 self.rx_data_from_slaves = temp_buffer
                 self.rx_data_from_slaves[1] -= 1
                 print("data from slave")
                 print(self.rx_data_from_slaves)
+            #end if
                         
                         
                 if self.rx_data_from_slaves[1] == 0:
@@ -78,5 +84,9 @@ class uart_interface:
                     print("forward to slave")
                     self.tx_data_to_master = self.rx_data_from_slaves
                     self.uart_from_master.write(self.tx_data_to_master)
+                # end if
+            #end if
+        #end if
                 
-        return 0, 0            
+        return 0, 0
+    #end def
