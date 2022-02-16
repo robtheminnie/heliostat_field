87edd1068d7ec27a5807f2aa6809ae74aed7c7c0
robtheminnie <47690074+robtheminnie@users.noreply.github.com>
2022-02-16 15:34:48 +0000

    Update pin_assignments.py

--- heliostat/software/pi_pico/micropython/pin_assignments.py
+++ heliostat/software/pi_pico/micropython/pin_assignments.py
@@ -2,11 +2,11 @@
 
 class pin_assignments(self):
   def __init__(self):
-    # uart to master
+    # uart to master, uart 0
     self.uart_from_master_TX = 0
     self.uart_to_master_RX = 1
 
-    # uart to slaves
+    # uart to slaves, uart 1
     self.uart_to_slaves_TX = 4
     self.uart_to_slaves_RX = 5
 
@@ -16,7 +16,6 @@ class pin_assignments(self):
     self.pan_coil_B = 12
     self.pan_coil_C = 13
     self.pan_coil_D = 14
-
     self.pan_min_stop = 15
     self.pan_max_stop = 16
 
@@ -26,8 +25,13 @@ class pin_assignments(self):
     self.tilt_coil_B = 18
     self.tilt_coil_C = 19
     self.tilt_coil_D = 20
-
     self.tilt_min_stop = 21
     self.tilt_max_stop = 22
+    
+    
+    # i2c port for imu, i2c 1
+    self.imu_sda = 2
+    self.imu_sdl = 3
+    
   #end def
 #end class
