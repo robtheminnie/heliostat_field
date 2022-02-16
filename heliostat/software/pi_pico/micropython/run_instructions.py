422e9313a94cc0276ef4b2d0b02720569ace5d06
robtheminnie <47690074+robtheminnie@users.noreply.github.com>
2022-02-16 10:02:06 +0000

    Update run_instructions.py

--- heliostat/software/pi_pico/micropython/run_instructions.py
+++ heliostat/software/pi_pico/micropython/run_instructions.py
@@ -1,4 +1,3 @@
-import uart
 import stepper
 
 
@@ -14,16 +13,19 @@ tilt_stepper = stepper.stepper(17, 18, 19, 20, 21, 22)
 def run_instructions(instruction, data):
   
   if instruction = instruction_ID.auto_home:
+    print("running auto home")
     # auto home both axis
     pan_stepper.auto_home()
     tilt_stepper.auto_home()
     
   elif instruction = instruction_ID.find_end_points:
+    print("finding end points")
     # find end points of both axis
     pan_stepper.find_end_points()
     tilt_stepper.find_end_points()
     
   elif instruction = instruction_ID.move_to_target:
+    print("moving to target")
     # move axis to target positions
     pan_stepper.move_to_target((data[0] << 8) | data[1])
     tilt_stepper.move_to_target((data[2] << 8) | data[3])
