6b56f5e81417c08d2a5189e29b10cc1717fe808b
robtheminnie <47690074+robtheminnie@users.noreply.github.com>
2022-02-17 22:16:40 +0000

    add imu angle to horizontal

--- heliostat/software/pi_pico/micropython/imu_driver.py
+++ heliostat/software/pi_pico/micropython/imu_driver.py
@@ -1,6 +1,7 @@
 from imu import MPU6050
 import pin_assignments
 import machine
+import math
 
 pin_assignments = pin_assignments.pin_assignment()
 
@@ -8,6 +9,7 @@ pin_assignments = pin_assignments.pin_assignment()
 i2c = machine.I2C(pin_assignments.imu_i2c_port, sda=machine.Pin(pin_assignments.imu_sda), scl=machine.Pin(pin_assignments.imu_sdl), freq=400000)
 imu = MPU6050(i2c)
 
+theta = 0
 
 def get_imu_data():
   
@@ -17,5 +19,7 @@ def get_imu_data():
   ay=round(imu.accel.y,4)
   az=round(imu.accel.z,4)
   tem=round(imu.temperature,2)
+  
+  theta = math.atan(math.sqrt((ax * ax) + (ay * ay)) / az) / (2 * 3.1415) * 360
 
-  print(ax,"\t",ay,"\t",az,"\t",tem,"        ",end="\r")
+  print(theta,"\t",tem,"        ",end="\r")
