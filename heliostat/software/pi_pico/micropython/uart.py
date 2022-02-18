d601339c1c1f11a5cb47a956fb01a63c654a4a72
robtheminnie <47690074+robtheminnie@users.noreply.github.com>
2022-02-18 15:42:58 +0000

    Update uart.py

--- heliostat/software/pi_pico/micropython/uart.py
+++ heliostat/software/pi_pico/micropython/uart.py
@@ -1,25 +1,13 @@
 import machine
 import utime
 import pin_assignments
+import instruction_ID
 
 # init pin_assignment class for enumerations
 pin_assignments = pin_assignments.pin_assignment()
 
-
-# class to define instruciton IDs, used like a enumeration (bluh!)
-class instruction_ID:
-    def __init__(self):
-        self.ndef = 0
-        self.get_ID = 1
-        self.auto_home = 2
-        self.find_end_points = 3
-        self.move_to_target = 4
-        self.auto_zero = 5
-    #end def
-    
-#end class
-
-instruction_ID = instruction_ID()
+# init instruction_ID class for enumerations
+instruction_ID = instruction_ID.instruction_ID()
 
 
 # class to define uart message structure
