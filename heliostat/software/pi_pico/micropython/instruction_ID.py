a9229d6874c0434404661b192ff3052c8b7cf253
robtheminnie <47690074+robtheminnie@users.noreply.github.com>
2022-02-18 15:43:12 +0000

    Create instruction_ID.py

---
+++ heliostat/software/pi_pico/micropython/instruction_ID.py
@@ -0,0 +1,13 @@
+
+# class to define instruciton IDs, used like a enumeration (bluh!)
+class instruction_ID:
+    def __init__(self):
+        self.ndef = 0
+        self.get_ID = 1
+        self.auto_home = 2
+        self.find_end_points = 3
+        self.move_to_target = 4
+        self.auto_zero = 5
+    #end def
+    
+#end class
